3105dd364bbd274d12354c18bbc3a7f13fd4bbc9
navarrooctavioa@gmail.com <navarrooctavioa@gmail.com>
2023-01-13 18:54:05 -0300

    Routing and NoteForm

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,14 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { Container } from 'react-bootstrap'
+import { NewNote } from './NewNote'
 
 function App() {
   
 
   return (
-    <div className="App">
-      Hello
-    </div>
+    <Container className='my-4'>
+      <Routes>
+        <Route path='/' element={<div>Home</div>}></Route>
+        <Route path='/new' element={<NewNote />}>New</Route>
+        <Route path='/*' element={<Navigate to='/' />}>New</Route>
+        <Route path='/:id'>
+          <Route index element={<h1>Show</h1>}/>
+          <Route path='edit' element={<h1>Edit</h1>}/>
+        </Route>
+      </Routes>
+    </Container>
   )
 }
 
